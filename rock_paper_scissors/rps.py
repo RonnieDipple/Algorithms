13d014e6c47ed6323be7d2df08ba4c73a20d0af4
ronniedipple <ronniedipple@gmail.com>
2020-02-20 16:50:25 -0600

    rps complete improved version

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -22,84 +22,57 @@
 
 import sys
 
-rock = "rock"
-paper = "paper"
-scissors = "scissors"
-initSet=[rock,paper,scissors]
-complete_list = rock+paper+scissors
-
-
+rock = ["rock"]
+paper = ["paper"]
+scissors = ["scissors"]
+available_options = ["rock", "paper", "scissors"]
 def rock_paper_scissors(n):
-  # rock = ["rock"]
-  # paper = ["paper"]
-  # scissors = ["scissors"]
-  # rps = [rock, paper, scissors]
+    if n == 0:
+        return [[]]
+    if n == 1:
+        return [rock, paper, scissors]
+    return rps_helper(n, rock) + rps_helper(n, paper) + rps_helper(n, scissors)
+def rps_helper(n, list):
+    if n == 1:
+        return [list]
+    rockList, paperList, scissorList = list[:], list[:], list[:]
+    rockList += rock
+    paperList += paper
+    scissorList += scissors
+    n -= 1
+    return (
+        rps_helper(n, rockList) + rps_helper(n, paperList) + rps_helper(n, scissorList)
+    )
+
+# rock = "rock"
+# paper = "paper"
+# scissors = "scissors"
+# initSet = [rock, paper, scissors]
+# complete_list = rock + paper + scissors
+
+
+#def rock_paper_scissors(n):
+
+
+
   # available_options = ['rock', 'paper', 'scissors']
-
-  # def rock_paper_scissors(n):
-  #   if n == 0:
-  #     return [[]]
-  #   if n == 1:
-  #     return rps
-  #   return rps_helper(n, rock) + rps_helper(n, paper) + rps_helper(n, scissors)
-  #
-  # def rps_helper(n, list):
-  #   if n == 1:
-  #     return list
-  #   if n == 2:
-  #     rockList = list.copy()
-  #     paperList = list.copy()
-  #     scissorList = list.copy()
-  #     rockList.extend(rock)
-  #     paperList.extend(paper)
-  #     scissorList.extend(scissors)
-  #     n -= 1
-  #     return [rps_helper(n, rockList), rps_helper(n, paperList), rps_helper(n, scissorList)]
+  # end_results = [] # Will result in a list of all the possible plays available.
+  # # Create Recursive Function
+  # # Using two parameters, rounds and match_results
+  # # rounds control how long our function will run for.
+  # # match_results will store the record of all the resulting plays
+  # def total_plays(rounds, match_result=[]):
+  #   # Base Case
+  #   # Once the loop is complete, add all of our plays to our end results
+  #   if rounds < 1:
+  #     return end_results.append(match_result)
   #   else:
-  #     rockList = list.copy()
-  #     paperList = list.copy()
-  #     scissorList = list.copy()
-  #     rockList.extend(rock)
-  #     paperList.extend(paper)
-  #     scissorList.extend(scissors)
-  #     n -= 1
-  #     return rps_helper(n, rockList) + rps_helper(n, paperList) + rps_helper(n, scissorList)
-  #
-  # print(rock_paper_scissors(6))
+  #     for option in available_options:
+  #       total_plays(rounds - 1, match_result + [option])
+  #       # Decrement our number of rounds each time
+  # total_plays(n) # Trigger the recursion
+  # return end_results
 
-  # 1. Understand the Question
-  # 2. Create Plan
-  # 3. Implement Plan
-  # 4. Revise and Improve
-  import sys
-  def rock_paper_scissors(n):
-    # Define all the possible play options
-    available_options = ['rock', 'paper', 'scissors']  # Three options its rps.
-    end_results = []  # Will result in a list of all the possible plays available.
-
-    # Create Recursive Function
-    # Using two parameters, rounds and match_results
-    # rounds control how long our function will run for.
-    # match_results will store the record of all the resulting plays
-    def total_plays(rounds, match_result=[]):
-      # Base Case
-      # Once the loop is complete, add all of our plays to our end results
-      if rounds < 1:
-        return end_results.append(match_result)
-      else:
-        for option in available_options:
-          total_plays(rounds - 1, match_result + [option])
-          # Decrement our number of rounds each time
-
-    total_plays(n)  # Trigger the recursion
-    return end_results
-
-  if __name__ == "__main__":
-    if len(sys.argv) > 1:
-      num_plays = int(sys.argv[1])
-      print(rock_paper_scissors(num_plays))
-    else:
-      print('Usage: rps.py [num_plays]')
 if __name__ == "__main__":
   if len(sys.argv) > 1:
     num_plays = int(sys.argv[1])
